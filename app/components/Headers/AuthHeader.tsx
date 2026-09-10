fab741026b9ea4ab35fb2f1f25d304bb41fd537e
Asmaa Aman <asmaa.aman91@gmail.com>
2026-09-11 00:48:37 +0300

    first commit

---
+++ app/components/Headers/AuthHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../../../assets/logo.svg";
+
+const AuthHeader = () => {
+  return (
+    <header>
+      <Logo alt="Logo" />
+    </header>
+  );
+};
+
+export default AuthHeader;
